c56765a4f6702d7b7621b5dccbc975b3e4c317da
Beata Karolczak <karolczak.beata@gmail.com>
2020-10-25 21:16:12 +0100

    Updates

---
+++ precache-manifest.f4c92265395483bcecd7b2f1944d716e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c930debf259f59a89fba8dcdd57e7f",
+    "url": "/chat-app-react/index.html"
+  },
+  {
+    "revision": "e7029f8390fc2ca12475",
+    "url": "/chat-app-react/static/css/main.7ae8bab8.chunk.css"
+  },
+  {
+    "revision": "53e1413778834042764e",
+    "url": "/chat-app-react/static/js/2.b66ec176.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/chat-app-react/static/js/2.b66ec176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7029f8390fc2ca12475",
+    "url": "/chat-app-react/static/js/main.4bb484e2.chunk.js"
+  },
+  {
+    "revision": "cf6d4b70a5f03eff2593",
+    "url": "/chat-app-react/static/js/runtime-main.e8160b5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat-app-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
